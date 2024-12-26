efe5fa235f7135f5f507be079ee986c1e9b820f1
fajarriskihidayat <fajarrh2002@gmail.com>
2024-12-26 11:07:45 +0700

    feat: crud api key

--- app/Filament/Resources/ApiKeyResource/Pages/CreateApiKey.php
+++ app/Filament/Resources/ApiKeyResource/Pages/CreateApiKey.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateApiKey extends CreateRecord
 {
     protected static string $resource = ApiKeyResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getURL('index');
+    }
 }
