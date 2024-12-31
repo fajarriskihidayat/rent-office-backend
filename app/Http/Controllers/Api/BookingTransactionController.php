8fc6a241017257ffd87c217024ed913ef797b2bf
fajarriskihidayat <fajarrh2002@gmail.com>
2024-12-31 10:21:22 +0700

    feat: check booking

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -5,13 +5,34 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBookingTransactionRequest;
 use App\Http\Resources\Api\BookingTransactionResource;
+use App\Http\Resources\Api\ViewBookingResource;
 use App\Models\BookingTransaction;
 use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
-    //
+    // mengecek data transaksi
+    public function booking_details(Request $request)
+    {
+        $request->validate([
+            'phone_number' => 'required|string',
+            'booking_trx_id' => 'required|string',
+        ]);
+
+        $booking = BookingTransaction::where('phone_number', $request->phone_number)
+            ->where('booking_trx_id', $request->booking_trx_id)
+            ->with(['officeSpace', 'officeSpace.city'])
+            ->first();
+
+        if (!$booking) {
+            return response()->json(['message' => 'booking not found'], 404);
+        }
+
+        return new ViewBookingResource($booking);
+    }
+
+    // menyimpan data transaksi
     public function store(StoreBookingTransactionRequest $request)
     {
         $validatedData = $request->validated();
